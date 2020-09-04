00ed5151dd18d0c9fa1d743bc53ce6601f4dc5a4
wsw <1098243691@qq.com>
2020-09-04 21:02:55 +0800

    20200904

--- backup.py
+++ backup.py
@@ -1,5 +1,4 @@
 #剑指Offer 07 重建二叉树
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -29,3 +28,45 @@ class Solution:
             root.left = self.buildTree(leftpreorder, leftinorder)
             root.right = self.buildTree(rightpreorder, rightinorder)
             return root
+
+
+#剑指Offer 06 从头到尾打印链表
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        if head is None:
+            return []
+        
+        temp = []
+        result = []
+        temp.append(head.val)
+
+        while head.next:
+            head = head.next
+            temp.append(head.val)
+
+        for i in range(len(temp)-1, -1, -1):
+            result.append(temp[i])
+
+        return result
+
+
+#剑指Offer 05 替换空格
+class Solution:
+    def replaceSpace(self, s: str) -> str:
+        result = ''
+        if s is None:
+            return None
+            
+        for i in s:
+            if i is not ' ':
+                result +=i
+            else:
+                result += '%20'
+        
+        return result
