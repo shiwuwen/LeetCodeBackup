c618729fc63b6dc849a173c1cb059af45960b098
wenshiwu <wenshiwu@kuaishou.com>
2021-07-05 15:09:44 +0800

    add coin change and edit distance

--- leetcode_coding.py
+++ leetcode_coding.py
@@ -1,4 +1,82 @@
 '''
+找零问题
+包含找零的策略
+coins：1，2，5
+amount：11
+'''
+def coinChange(coins, amount):
+    dp = [amount+1 for i in range(amount + 1)]
+
+    dp[0] = 0
+    change_list = []
+
+    for i in range(1, amount+1):
+        for coin in coins:
+            if i - coin < 0:
+                continue
+            dp[i] = min(dp[i], dp[i-coin] + 1)
+    
+    index = amount
+    while index:
+        for coin in coins:
+            if dp[index] == dp[index-coin] + 1:
+                change_list.append(coin)
+                index = index - coin
+                break
+
+    return dp[-1], change_list
+
+
+'''
+编辑距离
+包含具体修改方式
+intention execution 5
+horse ros 3
+'''
+def editdistance(s1, s2):
+    length1 = len(s1) + 1
+    length2 = len(s2) + 1
+    choice = ['same', 'delete', 'add', 'edit']
+    edit_list = []
+
+    dp = [[0 for j in range(length2)] for i in range(length1)]
+
+    for i in range(1, length1):
+        dp[i][0] = i
+
+    for j in range(1, length2):
+        dp[0][j] = j
+
+    for i in range(1, length1):
+        for j in range(1, length2):
+            if s1[i-1] == s2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = min(dp[i][j-1] + 1, dp[i-1][j] + 1, dp[i-1][j-1] + 1)
+
+    i = length1 - 1
+    j = length2 - 1
+    while i > 0 or j > 0:
+        if i > 0 and j > 0 and s1[i-1] == s2[j-1]:
+            edit_list.append('same')
+            i = i - 1
+            j = j - 1
+        else:
+            if dp[i][j] == dp[i][j-1] + 1:
+                edit_list.append('add')
+                j = j - 1
+            elif dp[i][j] == dp[i-1][j] + 1:
+                edit_list.append('delete')
+                i = i - 1
+            else:
+                edit_list.append('modify')
+                i = i - 1
+                j = j - 1
+
+    return dp[-1][-1], edit_list
+
+
+'''
 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 s = "abcabcbb" 3
 s = "bbbbb" 1
@@ -52,7 +130,7 @@ class Solution:
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def addTwoNumbers(self, l1, l2):
         head = curr = ListNode()
         carry_bit = 0
 
@@ -151,12 +229,33 @@ class Solution:
 
 
 if __name__ == '__main__':
+    # coin change
+    print('coin change')
+    coins = [1, 2, 5]
+    amount = 13
+    result, change_list = coinChange(coins, amount)
+    print(result)
+    print(change_list)
+    print()
+
+    # edit distance
+    print('edit distance')
+    s1 = 'intention'
+    s2 = 'execution'
+    result, edit = editdistance(s1, s2)
+    print(result)
+    print(s1)
+    print(s2)
+    edit.reverse()
+    print(edit)
+    print()
+
+    # two sum
+    print('two sum')
     # nums = [2,7,11,15]
     # target = 9
     nums = [3,2,4]
     target = 6
-
     test_class = Solution()
     result = test_class.twoSum(nums, target)
-
     print(result)
